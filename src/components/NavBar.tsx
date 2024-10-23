a846e11856da992944054024be600b641f93ec28
Emeric Lesage <elesage.dev@gmail.com>
2024-10-23 11:28:47 +0200

    index button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface Pokemon {
   name: string;
   imgSrc?: string;
@@ -9,30 +11,43 @@ interface NavBarProps {
   pokemonList: Pokemon[];
 }
 
-function NavBar({ pokemonIndex, setPokemonIndex, pokemonList }: NavBarProps) {
-  const handleclickPrevious = () => {
-    setPokemonIndex(pokemonIndex - 1);
-  };
-  const handleclickNext = () => {
-    setPokemonIndex(pokemonIndex + 1);
-  };
+function NavBar({ setPokemonIndex, pokemonList }: NavBarProps) {
   return (
     <>
-      {pokemonIndex > 0 ? (
-        <button type="button" onClick={handleclickPrevious}>
-          Précédent
+      {pokemonList.map((pokemon, index) => (
+        <button
+          key={pokemon.name}
+          type="button"
+          onClick={() => setPokemonIndex(index)}
+        >
+          {pokemon.name}
         </button>
-      ) : (
-        ""
-      )}
-      {pokemonIndex < pokemonList.length - 1 ? (
-        <button type="button" onClick={handleclickNext}>
-          Suivant
-        </button>
-      ) : (
-        ""
-      )}
+      ))}
     </>
   );
 }
+// const handleclickPrevious = () => {
+//   setPokemonIndex(pokemonIndex - 1);
+// };
+// const handleclickNext = () => {
+//   setPokemonIndex(pokemonIndex + 1);
+// };
+// return (
+//   <>
+//     {pokemonIndex > 0 ? (
+//       <button type="button" onClick={handleclickPrevious}>
+//         Précédent
+//       </button>
+//     ) : (
+//       ""
+//     )}
+//     {pokemonIndex < pokemonList.length - 1 ? (
+//       <button type="button" onClick={handleclickNext}>
+//         Suivant
+//       </button>
+//     ) : (
+//       ""
+//     )}
+//   </>
+// );
 export default NavBar;
